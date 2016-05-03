64dd3a4629b4c4dc7f8ad52c2c56f13bb8fa1523
Jonathan Alvarez-Gutierrez <jagthebeetle@gmail.com>
2016-05-02 23:08:14 -0400

    Remove ngroute initial transclusion animation

--- _includes/main.js
+++ _includes/main.js
@@ -1,20 +1,52 @@
-angular.module('jag',[
-    'ngRoute','ngAnimate'
-  ])
+angular.module('jag',['ngRoute','ngAnimate'])
   .config(function($routeProvider, $locationProvider) {
     $locationProvider.html5Mode({
       'enabled': true
     });
     $routeProvider
-        .when('/:post*', {
-          templateUrl: function(path) {
-            var suffixedPath = path.post.replace(/\.html$/, '-partial.html');
-            var isPartial = suffixedPath.indexOf('-partial.html') > -1;
-            return (isPartial)? '/partials/' + suffixedPath : suffixedPath + 'partial.html';
-          }
-        })
-        .otherwise('/');
+      .when('/:post*', {
+        templateUrl: function(path) {
+          var suffixedPath = path.post.replace(/\.html$/, '-partial.html');
+          var isPartial = suffixedPath.indexOf('-partial.html') > -1;
+          return (isPartial) ? 
+            '/partials/' + suffixedPath :
+            suffixedPath + 'partial.html';
+        }
+      });
   })
-  .controller('BlogController', BlogController);
+  .directive('routeExtend', function($document) {
+    return {
+      restrict: 'A',
+      link: function(scope, element) {
+        var onceToken;
 
-function BlogController($rootScope) {}
+        scope.$on('$routeChangeSuccess', updateDom);
+
+        function updateDom($event, newRoute) {
+          var template = newRoute && newRoute.locals && newRoute.locals.$template;
+          var title = getTitleFromTemplate(template);
+          if (title) {
+            $document[0].title = title + ' | ' + '{{ site.title }}';
+          } else {
+            $document[0].title = '{{ site.title }}';
+          }
+
+          onceToken = onceToken || Number.isInteger(
+            setTimeout(function() {
+              element.parent().addClass("anim-active")
+            }, 0)
+          );
+        }
+
+        function getTitleFromTemplate(str) {
+          if (str && str.match) {
+            var match = str.match(/<h1.*?>(.*?)<\/h1>/i);
+            if (match) {
+              return match[1];
+            }
+          }
+        }
+
+      }
+    };
+  });
